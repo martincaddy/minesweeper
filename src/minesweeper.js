aabee8302cb3b9d6bda0332e28b805170c98756b
Martin Caddy <martincaddy@gmail.com>
2017-12-05 19:44:32 +0000

    Added arrays for game board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,11 +1,18 @@
-const blankLine = ' | | ';
-const guessLine = '1| | ';
-const bombLine = ' |B| ';
-console.log('This is what an empty board would look like: ');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const exampleBoard = [
+  [' ',' ',' '],
+  [' ',' ',' '],
+  [' ',' ',' ']
+];
+
+const printBoard = board => {
+  console.log('Current board: ');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
+printBoard(exampleBoard);
+exampleBoard[0][1] = '1';
+exampleBoard[2][2] = 'B';
+
+printBoard(exampleBoard);

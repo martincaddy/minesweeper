ed4900a48337f3112a5855595fb268a7934c7716
Martin Caddy <martincaddy@gmail.com>
2017-12-06 16:37:58 +0000

    Built dynamic gameboard

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,18 +1,45 @@
-const exampleBoard = [
-  [' ',' ',' '],
-  [' ',' ',' '],
-  [' ',' ',' ']
-];
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+  const row = [];
+  for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
+    row.push('');
+  }
+  board.push(row);
+  }
+  return board;
+};
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+  const row = [];
+  for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
+    row.push(null);
+  }
+  board.push(row);
+}
+
+
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+    board[randomRowIndex][randomColumnIndex] = 'B';
+
+    numberOfBombsPlaced++;
+  }
+return board;
+};
 
 const printBoard = board => {
   console.log('Current board: ');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+  console.log(board.map (row => row.join(' | ')).join('\n'));
 };
 
-printBoard(exampleBoard);
-exampleBoard[0][1] = '1';
-exampleBoard[2][2] = 'B';
+const playerBoard = generatePlayerBoard(5, 5);
+const bombBoard = generateBombBoard(5, 5, 3);
 
-printBoard(exampleBoard);
+printBoard(playerBoard);
+printBoard(bombBoard);
